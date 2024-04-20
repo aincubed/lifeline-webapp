b8a0c576864a5aed94b5f1c4ebc0b76d0d8516a8
munozker <kerwinmunoz14@gmail.com>
2024-04-20 15:39:16 +0800

    update Sidenav

--- src/components/molecules/SideNav/SideNav.tsx
+++ src/components/molecules/SideNav/SideNav.tsx
@@ -8,6 +8,7 @@ interface ISideNavItem {
   path: string;
   icon: LucideIcon;
 }
+
 export const items: ISideNavItem[] = [
   {
     name: "Dashboard",
@@ -28,25 +29,25 @@ export const items: ISideNavItem[] = [
 
 export default function SideNav() {
   return (
-    <div className="fixed left-0 top-0 z-10 h-[1280px] w-64 space-y-[160%] bg-gray-300">
-      <div>
+    <div className="fixed left-0 top-0 z-10 flex h-screen w-64 flex-col bg-gray-300">
+      <div className="flex flex-grow flex-col">
         <div className="flex h-48 flex-col justify-center text-center">
           <h1 className="text-3xl font-bold">LIFELINE</h1>
           <p className="text-lg">Donor List Repository</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-grow flex-col">
           {items.map((item) => (
             <SideNavItem key={item.path} item={item} />
           ))}
         </div>
       </div>
       <div className="flex flex-col space-y-10 pl-3">
-        <ul className="flex flex-col space-y-5 text-lg font-semibold">
-          <li className="flex items-center space-x-5 pl-7">
+        <ul className="mb-[30px] ml-[16px] flex flex-col space-y-5 text-lg font-semibold">
+          <li className="flex items-center space-x-5">
             <BadgeInfo />
             <Link href="/">Help?</Link>
           </li>
-          <li className="flex items-center space-x-5 pl-7">
+          <li className="flex items-center space-x-5">
             <LogOut />
             <Link href="/">Sign out</Link>
           </li>
