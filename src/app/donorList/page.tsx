9bd222d3936d6d488a1e95aece62b6991f055fb5
eroscarter20 <erctermulo2002@gmail.com>
2024-04-22 21:42:29 +0800

    Created the table for donor's info

--- src/app/donorList/page.tsx
+++ src/app/donorList/page.tsx
@@ -1,3 +1,48 @@
-export default function DonorList() {
-  return <div>DonorList</div>;
+"use client";
+import { DataTable } from "../../components/molecules/listDonor/DataTable";
+
+import {
+  DonorInfo,
+  columns,
+} from "../../components/molecules/listDonor/column";
+
+async function getData(): Promise<DonorInfo[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      lastName: "Termulo",
+      firstName: "Erica",
+      bloodGroup: "O",
+      acquisitionDate: new Date("2024-04-22T12:30:00"),
+    },
+    {
+      id: "728ed52h",
+      lastName: "Bergola",
+      firstName: "Khryx Rhoien",
+      bloodGroup: "B",
+      acquisitionDate: new Date("2024-03-22T12:30:00"),
+    },
+    {
+      id: "728ed52g",
+      lastName: "Gatus",
+      firstName: "Mark Andrei",
+      bloodGroup: "O",
+      acquisitionDate: new Date("2024-02-22T12:30:00"),
+    },
+    // ...
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <div className="mb-5 text-center">
+        <h1 className="text-2xl font-bold">Donor Information</h1>
+      </div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
