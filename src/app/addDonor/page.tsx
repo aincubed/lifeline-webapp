45d7a7456ff21e92f4c6e8c2023322049299d4c0
munozker <kerwinmunoz14@gmail.com>
2024-04-24 09:05:07 +0800

    fixed the format

--- src/app/addDonor/page.tsx
+++ src/app/addDonor/page.tsx
@@ -21,40 +21,56 @@ import { Separator } from "@/components/ui/separator";
 
 export default function AddDonor() {
   return (
-    <div className="flex flex-col items-center px-14 pb-10 pt-20  text-3xl font-bold">
-      <h1 className="pb-8">ADD DONOR</h1>
+    <div className="max-w-[1280px] p-[50px]">
+      <h1 className="mb-[10px] text-[30px] font-bold">ADD DONOR</h1>
 
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex grid grid-cols-6 items-center justify-center gap-x-8 gap-y-4 pb-5">
-          <div className="col-span-2 col-start-1 grid w-full min-w-full max-w-lg items-center gap-1.5">
+      <div>
+        <div className="grid w-full shrink-0 gap-[20px] md:grid-cols-3">
+          <div>
             <Label>Last Name</Label>
-            <Input id="lastName" placeholder="Last Name" />
+            <Input
+              className="border-black"
+              id="lastName"
+              placeholder="Last Name"
+            />
           </div>
 
-          <div className="col-span-2 col-start-3 grid w-full min-w-full max-w-lg items-center gap-1.5">
+          <div>
             <Label>First Name</Label>
-            <Input id="firstName" placeholder="First Name" />
+            <Input
+              className="border-black"
+              id="firstName"
+              placeholder="First Name"
+            />
           </div>
 
-          <div className="col-span-2 col-start-5 grid w-full min-w-full max-w-lg items-center gap-1.5">
+          <div>
             <Label>Middle Name</Label>
-            <Input id="middleName" placeholder="Middle Name" />
+            <Input
+              className="border-black"
+              id="middleName"
+              placeholder="Middle Name"
+            />
           </div>
 
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Birthday</Label>
-            <Input id="birthday" placeholder="dd/mm/yyyy" />
+            <Input
+              className="border-black"
+              id="birthday"
+              placeholder="dd/mm/yyyy"
+            />
           </div>
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Age</Label>
-            <Input id="age" placeholder="" />
+            <Input className="border-black" id="age" />
           </div>
 
-          <div className="col-span-2 col-start-4 grid w-full min-w-full max-w-sm gap-1.5">
+          <div>
             <Label>Civil Status</Label>
             <Select>
-              <SelectTrigger className="w-full">
+              <SelectTrigger className="w-full border-black">
                 <SelectValue placeholder="Select" />
               </SelectTrigger>
               <SelectContent>
@@ -66,10 +82,10 @@ export default function AddDonor() {
             </Select>
           </div>
 
-          <div className="col-span-1 grid w-full min-w-full max-w-sm gap-1.5">
+          <div>
             <Label>Sex</Label>
             <Select>
-              <SelectTrigger className="w-full">
+              <SelectTrigger className="w-full border-black">
                 <SelectValue placeholder="Select" />
               </SelectTrigger>
               <SelectContent>
@@ -78,88 +94,84 @@ export default function AddDonor() {
               </SelectContent>
             </Select>
           </div>
+        </div>
 
-          {/* Address */}
-          <div className="col-start-1 grid w-full max-w-sm items-center gap-1.5">
-            <Label className="pt-2 text-2xl">Address</Label>
+        <h1 className="mb-[10px] mt-[20px] text-[30px] font-bold">Address</h1>
+
+        <div className="grid w-full shrink-0 gap-[20px] md:grid-cols-3">
+          <div>
+            <Label>Number-test--</Label>
+            <Input className="border-black" id="addressNumber" />
           </div>
 
-          <div className="col-span-2 col-start-1 grid w-full max-w-sm items-center gap-1.5">
-            <Label>Number</Label>
-            <Input id="addressNumber" placeholder="" />
-          </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Street</Label>
-            <Input id="addressStreet" placeholder="" />
+            <Input className="border-black" id="addressStreet" />
           </div>
 
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Barangay</Label>
-            <Input id="addressBarangay" placeholder="" />
+            <Input className="border-black" id="addressBarangay" />
           </div>
 
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>City</Label>
-            <Input id="addressCity" placeholder="" />
+            <Input className="border-black" id="addressCity" />
           </div>
 
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Zip Code</Label>
-            <Input id="addressZipcode" placeholder="" />
+            <Input className="border-black" id="addressZipcode" />
           </div>
-
-          <div className="col-span-6 col-start-1 grid w-full max-w-4xl items-center gap-1.5 pb-4">
+        </div>
+        <div className="my-[70px] grid w-full shrink-0 gap-[20px] md:grid-cols-3">
+          <div className="col-span-2">
             <Label>Office Address</Label>
-            <Input id="addressOffice" placeholder="" />
+            <Input className="border-black" id="addressOffice" />
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Religion</Label>
-            <Input id="religion" placeholder="" />
+            <Input className="border-black" id="religion" />
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Citizenship</Label>
-            <Input id="citizenship" placeholder="" />
+            <Input className="border-black" id="citizenship" />
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm gap-1.5">
+          <div>
             <Label>Education</Label>
             <Select>
-              <SelectTrigger className="w-full">
+              <SelectTrigger className="w-full border-black">
                 <SelectValue placeholder="Select" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="HSgrad">High School Graduate</SelectItem>
-                <SelectItem value="COLgrad">College Graduate</SelectItem>
-                <SelectItem value="BDgrad">Bachelors Degree</SelectItem>
-                <SelectItem value="MASgrad">Masters Degree</SelectItem>
-                <SelectItem value="DOCgrad">Doctorate</SelectItem>
-                <SelectItem value="NAgrad">Not Applicable</SelectItem>
+                <SelectItem value="hsgrad">High School Graduate</SelectItem>
+                <SelectItem value="colgrad">College Graduate</SelectItem>
               </SelectContent>
             </Select>
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
+            <Label>Occupation</Label>
+            <Input className="border-black" id="workOccupation" />
+          </div>
+        </div>
+        <div className="mb-[100px] grid w-full shrink-0 gap-[20px] md:grid-cols-3">
+          <div>
             <Label>Telephone Number</Label>
-            <Input id="telephoneNumber" placeholder="" />
+            <Input className="border-black" id="telephoneNumber" />
           </div>
 
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Mobile Number</Label>
-            <Input id="mobileNumber" placeholder="" />
+            <Input className="border-black" id="mobileNumber" />
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>Email Address</Label>
-            <Input id="emailAddress" placeholder="" />
+            <Input className="border-black" id="emailAddress" />
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm gap-1.5">
+          <div>
             <Label>ID Type</Label>
             <Select>
-              <SelectTrigger className="w-full">
+              <SelectTrigger className="w-full border-black">
                 <SelectValue placeholder="Select ID" />
               </SelectTrigger>
               <SelectContent>
@@ -172,40 +184,30 @@ export default function AddDonor() {
               </SelectContent>
             </Select>
           </div>
-
-          <div className="col-span-2 grid w-full max-w-sm items-center gap-1.5">
+          <div>
             <Label>ID Number</Label>
-            <Input id="idnumber" placeholder="" />
+            <Input className="border-black" id="idnumber" />
           </div>
         </div>
+      </div>
 
-        <div>
-          <Card className="min-w-[700px]">
-            <CardHeader className="flex grid grid-cols-2 items-center gap-x-8">
-              <CardTitle>
-                Run Program
-                {/* <CardDescription>Card Description</CardDescription> */}
-              </CardTitle>
-              <Button variant="outline">Start</Button>
-            </CardHeader>
-
-            <Separator className="my-0.5" />
-
-            <CardContent className="flex items-center justify-center pt-5">
-              <Card className="flex min-h-[400px] min-w-[600px]">Camera</Card>
-
-              {/* <p>Card Content</p> */}
+      <div>
+        <Card className="flex flex-col items-center justify-center">
+          <CardHeader className="grid grid-cols-2 items-center">
+            <CardTitle>Run Program</CardTitle>
+            <Button variant="outline">Start</Button>
+          </CardHeader>
+          <Separator className="my-0.5" />
+          <Card className="mt-[15px] flex h-[500px] w-[700px] items-center justify-center border-[1px] border-dashed border-black">
+            <CardContent className="flex h-full w-full flex-col items-center justify-center ">
+              test
             </CardContent>
-
-            <CardFooter className="flex items-center justify-center gap-1.5 px-2">
-              {/* <p>Card Footer</p> */}
-              <Button variant="outline" className="min-w-32">
-                OPEN CAMERA
-              </Button>
-              <Button className="min-w-32">CAPTURE</Button>
-            </CardFooter>
           </Card>
-        </div>
+          <CardFooter className="mt-[10px] flex items-center justify-center px-2">
+            <Button className="min-w-32">OPEN CAMERA</Button>
+            <Button className="min-w-32">CAPTURE</Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
