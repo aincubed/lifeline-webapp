3a135e267bb35c4420e2572d0c717e278fad6523
munozker <kerwinmunoz14@gmail.com>
2024-04-26 00:57:26 +0800

    tama na muna to

---
+++ prisma/migrations/20240425164515_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "DonorInformation" (
+    "id" TEXT NOT NULL,
+    "Last Name (2)" TEXT NOT NULL,
+    "First Name (3)" TEXT NOT NULL,
+    "Middle Name (4)" TEXT,
+    "AGE (5)" INTEGER NOT NULL,
+    "SEX (6)" TEXT NOT NULL,
+
+    CONSTRAINT "DonorInformation_pkey" PRIMARY KEY ("id")
+);
