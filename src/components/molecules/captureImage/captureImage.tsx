231b14c6a1cd4d2a79a67a9dd2a79d228f859495
Ain <ainsquared@gmail.com>
2024-04-27 12:26:39 +0800

    camera

---
+++ src/components/molecules/captureImage/captureImage.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { useCallback, useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user",
+};
+
+export const CaptureImage = () => {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+      {isCaptureEnable || (
+        <Button variant={"outline"} onClick={() => setCaptureEnable(true)}>
+          start
+        </Button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <Button variant={"outline"} onClick={() => setCaptureEnable(false)}>
+              end
+            </Button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          </div>
+          <Button variant={"outline"} onClick={capture}>
+            capture
+          </Button>
+        </>
+      )}
+      {url && (
+        <>
+          {/* <div>
+            <button
+              onClick={() => {
+                setUrl(null);
+              }}
+            >
+              delete
+            </button>
+          </div> */}
+          <div>
+            <Image
+              src={url}
+              width={720}
+              height={360}
+              alt="Screenshot of sample image"
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
