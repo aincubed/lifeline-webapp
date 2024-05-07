0126f5fc113bcabd53cbb3c2fd17bfbc06ca6898
munozker <120777795+munozker@users.noreply.github.com>
2024-05-07 12:11:33 +0800

    Revert "Kerwinmunoz14/gr1 supabase db1"

--- src/app/NewDonor/page.tsx
+++ src/app/NewDonor/page.tsx
@@ -1,19 +1,8 @@
 import { Card, CardContent } from "@/components/ui/card";
 
 import { CaptureImage } from "@/components/molecules/CaptureImage";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
-export default async function AddDonor() {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    redirect("/login");
-  }
+export default function AddDonor() {
   return (
     <>
       <Card>
