f967ed086170cf24330b6cabaa9c11d2057c687d
munozker <kerwinmunoz14@gmail.com>
2024-05-10 00:32:12 +0800

    added features

--- src/components/molecules/User.tsx
+++ src/components/molecules/User.tsx
@@ -18,7 +18,7 @@
 
 //   return (
 //     session && (
-//       <div className="flex bg-blue text-[1.5rem] text-white p-10 rounded-md mb-5 items-center gap-4">
+//       <div className="mb-5 flex items-center gap-4 rounded-md bg-blue p-10 text-[1.5rem] text-white">
 //         Hey, Admin!
 //         <form action={signOut}>
 //           <button className="bg-btn-background hover:bg-btn-background-hover rounded-md px-4 py-2 no-underline">
