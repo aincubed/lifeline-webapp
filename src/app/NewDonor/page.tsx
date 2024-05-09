a1368ab06375cf4b9e75c1189b2048150cbe25d3
Ain <ainsquared@gmail.com>
2024-05-09 22:54:22 +0800

    major fixes and modifications in UI and build

--- src/app/NewDonor/page.tsx
+++ src/app/NewDonor/page.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
-import { onSubmit } from "@/app/api/addDonor/route";
-import CaptureImage from "@/components/molecules/CaptureImage";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+
+import CaptureImage from "@/components/molecules/CaptureImage";
+import { onSubmit } from "@/components/molecules/Submit";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 enum Gender {
   Male = "Male",
@@ -16,7 +18,7 @@ enum BloodGroup {
   B = "B",
   AB = "AB",
   O = "O",
-  Undefined = "Undefined",
+  Undefined = "undefined",
 }
 
 type FormData = {
@@ -56,15 +58,44 @@ export default function NewDonor() {
         errors={errors}
       />
       {submittedData && (
-        <div>
-          <h2>Submitted Data</h2>
-          <p>First Name: {submittedData.firstName}</p>
-          <p>Last Name: {submittedData.lastName}</p>
-          <p>Middle Name: {submittedData.middleName}</p>
-          <p>Sex: {submittedData.sex}</p>
-          <p>Age: {submittedData.age}</p>
-          <p>Blood Group: {submittedData.bloodGroup}</p>
-          <p>Practitioner Name: {submittedData.practitionerName}</p>
+        <div className="container mx-auto">
+          <Alert className="mb-5">
+            <AlertTitle className="pb-5">
+              Please review the following data before proceeding:
+            </AlertTitle>
+            <AlertDescription>
+              <div className="flex text-[17px] gap-20">
+                <div className="">
+                  <div className="flex gap-x-4">
+                    <p>First Name:</p>
+                    <p className="font-bold"> {submittedData.firstName}</p>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <p>Last Name:</p>
+                    <p className="font-bold">{submittedData.lastName}</p>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <p>Middle Name:</p>
+                    <p className="font-bold">{submittedData.middleName}</p>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex gap-x-4">
+                    <p>Sex:</p>
+                    <p className="font-bold">{submittedData.sex}</p>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <p>Age:</p>
+                    <p className="font-bold">{submittedData.age}</p>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <p>Blood Group:</p>
+                    <p className="font-bold">{submittedData.bloodGroup}</p>
+                  </div>
+                </div>
+              </div>
+            </AlertDescription>
+          </Alert>
           <CaptureImage
             firstName={submittedData.firstName}
             lastName={submittedData.lastName}
@@ -84,121 +115,138 @@ type MyFormProps = {
 
 function MyForm({ onSubmit, register, handleSubmit, errors }: MyFormProps) {
   return (
-    <div className="max-w-md mx-auto p-4">
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-medium mb-5">Donor Directory</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-        <div>
-          <label
-            htmlFor="firstName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            First Name:
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            {...register("firstName", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          />
-          {errors.firstName && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
+        <div className="flex flex-grow gap-x-3">
+          <div className="grow">
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name:
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              {...register("firstName", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+            {errors.firstName && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="grow">
+            <label
+              htmlFor="middleName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Middle Name:
+            </label>
+            <input
+              type="text"
+              id="middleName"
+              {...register("middleName", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+            {errors.middleName && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="grow">
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Last Name:
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              {...register("lastName", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+            {errors.lastName && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
         </div>
-        <div>
-          <label
-            htmlFor="middleName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Middle Name:
-          </label>
-          <input
-            type="text"
-            id="middleName"
-            {...register("middleName", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          />
-          {errors.middleName && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="lastName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Last Name:
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            {...register("lastName", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          />
-          {errors.lastName && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="age"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Age:
-          </label>
-          <input
-            type="number"
-            id="age"
-            {...register("age", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          />
-          {errors.age && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="sex"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Sex:
-          </label>
-          <select
-            id="sex"
-            {...register("sex", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          >
-            <option value="">Select Sex</option>
-            {Object.values(Gender).map((value) => (
-              <option key={value} value={value}>
-                {value}
-              </option>
-            ))}
-          </select>
-          {errors.sex && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="bloodGroup"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Blood Group:
-          </label>
-          <select
-            id="bloodGroup"
-            {...register("bloodGroup", { required: true })}
-            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-          >
-            <option value="">Select Blood Group</option>
-            {Object.values(BloodGroup).map((value) => (
-              <option key={value} value={value}>
-                {value}
-              </option>
-            ))}
-          </select>
-          {errors.bloodGroup && (
-            <span className="text-red-500 text-sm">This field is required</span>
-          )}
+        <div className="flex flex-grow gap-x-3">
+          <div className="grow">
+            <label
+              htmlFor="age"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Age:
+            </label>
+            <input
+              type="number"
+              id="age"
+              {...register("age", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            />
+            {errors.age && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="grow">
+            <label
+              htmlFor="sex"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Sex:
+            </label>
+            <select
+              id="sex"
+              {...register("sex", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            >
+              <option value="">Select Sex</option>
+              {Object.values(Gender).map((value) => (
+                <option key={value} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+            {errors.sex && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
+          <div className="grow">
+            <label
+              htmlFor="bloodGroup"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Blood Group:
+            </label>
+            <select
+              id="bloodGroup"
+              {...register("bloodGroup", { required: true })}
+              className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+            >
+              <option value="">Select Blood Group</option>
+              {Object.values(BloodGroup).map((value) => (
+                <option key={value} value={value}>
+                  {value}
+                </option>
+              ))}
+            </select>
+            {errors.bloodGroup && (
+              <span className="text-red-500 text-sm">
+                This field is required
+              </span>
+            )}
+          </div>
         </div>
         <div>
           <label
@@ -220,7 +268,7 @@ function MyForm({ onSubmit, register, handleSubmit, errors }: MyFormProps) {
         <div>
           <button
             type="submit"
-            className="bg-black text-white px-4 py-2 rounded-md"
+            className="border border-input bg-background hover:bg-accent hover:text-accent-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2"
           >
             Submit
           </button>
--- src/app/ScreenBlood/page.tsx
+++ src/app/ScreenBlood/page.tsx
@@ -1,5 +1,39 @@
+"use client";
+
 import CaptureImage from "@/components/molecules/CaptureImage";
 
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
 export default function ScreenBlood() {
-  return <CaptureImage />;
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-medium mb-5">Blood Screening</h1>
+      <Alert
+        variant="destructive"
+        className={cn("block mb-5", { hidden: isCaptureEnable })}
+      >
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Emergency Use Only</AlertTitle>
+        <AlertDescription>
+          This function is specifically used for EMERGENCY SCREENINGS ONLY.
+          Please adhere to the instructed process if not necessary.
+        </AlertDescription>
+      </Alert>
+      <Button
+        variant={"destructive"}
+        className={cn("flex mb-5", { hidden: isCaptureEnable })}
+        onClick={() => setCaptureEnable(true)}
+      >
+        I undestand
+      </Button>
+      {isCaptureEnable && <CaptureImage />}
+    </div>
+  );
 }
