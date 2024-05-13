1bbd26638541040fd18ec24092d8f831b93d494d
munozker <kerwinmunoz14@gmail.com>
2024-05-13 10:09:06 +0800

    fixed donor count

--- src/app/donorList/page.tsx
+++ src/app/donorList/page.tsx
@@ -1,18 +1,14 @@
-// Importing DataTable component from the listDonor directory
 import { createClient } from "@/utils/supabase/server";
 import { DataTable } from "../../components/molecules/listDonor/DataTable";
 
-// Importing DonorInfo type and columns definition from listDonor directory
 import {
   DonorInfo,
   columns,
 } from "../../components/molecules/listDonor/column";
 
 const supabase = createClient();
-// Function to asynchronously fetch donor data
 async function getData(): Promise<DonorInfo[]> {
   try {
-    // Fetch specific columns from the 'donor_info' table in Supabase
     const { data, error } = await supabase
       .from("todos")
       .select(
@@ -21,7 +17,7 @@ async function getData(): Promise<DonorInfo[]> {
     if (error) {
       throw error;
     }
-    // Return the fetched data
+
     return data;
   } catch (error) {
     console.error("Error fetching data:", (error as Error).message);
@@ -29,12 +25,8 @@ async function getData(): Promise<DonorInfo[]> {
   }
 }
 
-// Default function for the DemoPage component
 export default async function DemoPage() {
-  // Asynchronously fetch donor data
   const data = await getData();
-
-  // Rendering DemoPage component
   return (
     <div className="container mx-auto py-10">
       <div className="mb-5 text-center">
